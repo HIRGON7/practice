ef597747e1cb177ff3915ae239a5961cf0938497
hadi jawhari <126268930+HIRGON7@users.noreply.github.com>
2026-07-27 00:32:19 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='bg-blue-600 p-4 text-white flex justify-between items-center'>
-        <h1 className='text-xl font-bold'> My App</h1>
+        <h1 className='text-xl font-bold transition-transform duration-300 ease-in-out hover:scale-130 '> My App</h1>
         <ul className='flex space-x-5'>
         <li> 
             <a href="/" className=' hover:text-emerald-800'> Home</a>
@@ -15,7 +15,7 @@ const Navbar = () => {
             <a href="/communities" className='hover:text-orange-600'> Communities</a>
         </li>
          <li> 
-            <a href="/topics" className='hover:text-shadow-teal-500'> Topics</a>
+            <a href="/topics" className='hover:text-mauve-950'> Topics</a>
         </li>
          <li> 
             <a href="/developers" className='hover:text-amber-300'> Developers</a>
